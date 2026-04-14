e8f2de91c5b73f5fad6f4e7e8ab3f27a602614fc
Vaidehi Pate <120419659+vaidehipate@users.noreply.github.com>
2026-04-14 21:38:44 +0530

    Create 1768. Merge Strings Alternately.java

---
+++ String/1768. Merge Strings Alternately.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder ans = new StringBuilder();
+        int n1 = word1.length();
+        int n2 = word2.length();
+        int i = 0;
+
+        while (i < n1 || i < n2) {
+            if (i < n1) {
+                ans.append(word1.charAt(i));
+            }
+            if (i < n2) {
+                ans.append(word2.charAt(i));
+            }
+            i++;
+        }
+        return ans.toString();
+    }
+}
+
+"""
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder ans = new StringBuilder();
+        int n1 = word1.length();
+        int n2 = word2.length();
+        int i = 0;
+
+        while (i < n1 || i < n2) {
+            if (i < n1) {
+                ans.append(word1.charAt(i));
+            }
+            if (i < n2) {
+                ans.append(word2.charAt(i));
+            }
+            i++;
+        }
+        return ans.toString();
+    }
+}
